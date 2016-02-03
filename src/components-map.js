009ce0f2333d1c2f4e38c985e74af1622e159c55
me6iaton <me6iaton@gmail.com>
2016-02-03 18:18:24 +0300

    change main in paskage.json, fix components map set

--- src/components-map.js
+++ src/components-map.js
@@ -2,7 +2,7 @@ let map = new Map();
 
 export default {
   set(key, value) {
-    if (map.has(key) {
+    if (map.has(key)) {
       console.error(`${key} already exists in components map`)
     }
     return map.set(key, value)
